f01321e2c45b24b652a77a54522608d399a77d34
RAVI KUMAR <aims.ravi1609@gmail.com>
2014-09-15 16:55:10 +0530

    package name has changed from controllers to actions

---
+++ MediSys/src/com/medisys/ravi/actions/LoginController.java
@@ -0,0 +1,51 @@
+package com.medisys.ravi.actions;
+
+import com.medisys.ravi.beans.LoginBean;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class LoginController extends ActionSupport implements ModelDriven<LoginBean>
+{
+
+	/**
+	 * 
+	 * this class is developed for the login of the users
+	 */
+	private static final long serialVersionUID = 1L;
+// fields 
+	private LoginBean login_bean;
+	
+	//getters and setters 
+	
+	public LoginBean getLogin_bean() {
+		return login_bean;
+	}
+
+	public void setLogin_bean(LoginBean login_bean) {
+		this.login_bean = login_bean;
+	}
+	
+	
+	
+	@Override
+	public LoginBean getModel() {
+		// TODO Auto-generated method stub
+		return login_bean;
+	}
+
+
+
+	public String LoginUser()
+	{
+		
+		
+		
+		
+		return SUCCESS;
+	}
+	
+	
+	
+	
+	
+}
