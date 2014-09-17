a3eeb44576e04dfc4df435445ac44bb78c8f797a
RAVI KUMAR <ravi1609@users.noreply.github.com>
2014-09-17 20:48:34 +0530

    Creating the UserRegistrationBean Class
    Committing the UserRegistration class for the registration purpose

---
+++ MediSys/src/com/medisys/ravi/beans/UserRegistrationBean.java
@@ -0,0 +1,48 @@
+package com.medisys.ravi.beans;
+import java.io.*;
+public class UserRegistrationBean implements Serializable {
+
+
+//fields
+private String first_name;
+private String sur_name;
+private String mobile_no;
+private String email;
+private String address_one;
+private String address_two;
+private String address_three;
+private String gender;
+private String alternate_mobile_no;
+private String 
+
+
+// generate getters and setters of the fields 
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
