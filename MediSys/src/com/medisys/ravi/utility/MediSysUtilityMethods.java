035b09bdb203f2fa1215af2ee61f233ef53a2adb
RAVI KUMAR <ravi1609@users.noreply.github.com>
2016-11-30 20:49:13 +0530

    adding the mail utility method

--- MediSys/src/com/medisys/ravi/utility/MediSysUtilityMethods.java
+++ MediSys/src/com/medisys/ravi/utility/MediSysUtilityMethods.java
@@ -2585,5 +2585,84 @@ public class MediSysUtilityMethods {
 							
 						}
 	
+	// this is the method written for the sms and email utility
+	// here 0 refers to the unsuccessfull mail sending 
+	//1 means the successfull mail sending
+	public static int sendEmailToUser(String message,String subject,String message_to,String message_from){
+	 final String FROM = "admin@www.medisysonline.COM";   // Replace with your "From" address. This address must be verified.
+         final String TO = "aims.ravi1609@gmail.com";  // Replace with a "To" address. If your account is still in the 
+                                                       // sandbox, this address must be verified.
+    
+     final String BODY = "This email was sent through the Amazon SES SMTP interface by using Java.";
+     final String SUBJECT = "Amazon SES test (SMTP interface accessed using Java)";
+    
+    // Supply your SMTP credentials below. Note that your SMTP credentials are different from your AWS credentials.
+     final String SMTP_USERNAME = "AKIAIOXBOFNII4GIGNWA";  // Replace with your SMTP username.
+     final String SMTP_PASSWORD = "AtuH4vzZb7frUwqD830q8ziLazkTEYknanr495uVLCVl";  // Replace with your SMTP password.
+    
+    // Amazon SES SMTP host name. This example uses the US West (Oregon) region.
+     final String HOST = "email-smtp.us-east-1.amazonaws.com";    
+    
+    // The port you will connect to on the Amazon SES SMTP endpoint. We are choosing port 25 because we will use
+    // STARTTLS to encrypt the connection.
+     final int PORT = 25;
+	int sending_flag=0;
+	
+		// starting mail sending 
+		
+		    // Create a Properties object to contain connection configuration information.
+    	Properties props = System.getProperties();
+    	props.put("mail.transport.protocol", "smtps");
+    	props.put("mail.smtp.port", PORT); 
+    	
+    	// Set properties indicating that we want to use STARTTLS to encrypt the connection.
+    	// The SMTP session will begin on an unencrypted connection, and then the client
+        // will issue a STARTTLS command to upgrade to an encrypted connection.
+    	props.put("mail.smtp.auth", "true");
+    	props.put("mail.smtp.starttls.enable", "true");
+    	props.put("mail.smtp.starttls.required", "true");
+
+        // Create a Session object to represent a mail session with the specified properties. 
+    	Session session = Session.getDefaultInstance(props);
+
+        // Create a message with the specified information. 
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(FROM));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
+        msg.setSubject(SUBJECT);
+        msg.setContent(BODY,"text/plain");
+            
+        // Create a transport.        
+        Transport transport = session.getTransport();
+                    
+        // Send the message.
+        try
+        {
+            System.out.println("Attempting to send an email through the Amazon SES SMTP interface...");
+            
+            // Connect to Amazon SES using the SMTP username and password you specified above.
+            transport.connect(HOST, SMTP_USERNAME, SMTP_PASSWORD);
+        	
+            // Send the email.
+            transport.sendMessage(msg, msg.getAllRecipients());
+            sending_flag=1;
+		System.out.println("Email sent!");
+		
+        }
+        catch (Exception ex) {
+            System.out.println("The email was not sent.");
+            System.out.println("Error message: " + ex.getMessage());
+		sending_flag=0;
+        }
+        finally
+        {
+            // Close and terminate the connection.
+            transport.close();        	
+        }		
+	}
+	
+	return sending_flag;
+	
+	
 	
 }
